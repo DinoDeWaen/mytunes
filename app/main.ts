783765e8e7d7afb72ecf6e645d2bfa71b4d7b0b0
Dino De Waen <dinodewaen@gmail.com>
2016-06-05 11:00:06 +0200

    continue setup

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 import ApplicationComponent from 'app/components/application/application';
 
-bootstrap(ApplicationComponent);
+bootstrap(<any>ApplicationComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
